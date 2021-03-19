7c4146ac291db76b9deeae6c4891e62dd95534d5
Mariia Stykhylias <stihilia@gmail.com>
2021-03-19 20:34:20 +0200

    first commit

---
+++ fibonacci_recursive/fibonacci_recursive/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace fibonacci_iteration
+{
+    class Program
+    {
+        static long Fib(int n)
+        {
+            if (n == 0)
+                return 0;
+            if (n == 1)
+                return 1;
+            return Fib(n - 1) + Fib(n - 2);
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Fib(6) = " + Fib(6));
+        }
+    }
+}
